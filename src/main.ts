b2d797f5bbd24af2127a73294d4795ea425fe963
pleasureswx123 <pleasureswx123@163.com>
2025-03-08 14:18:01 +0800

    feat: 全局注册Icon组件

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { Icon } from '@iconify/vue';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// 全局注册 Icon 组件
+app.component('Icon', Icon);
 
 app.mount('#app')
